21dd598366f992462e29bb0ad7d0e16372732603
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-20 13:19:03 +0530

    Update zee5_dl.py

--- plugins/zee5_dl.py
+++ plugins/zee5_dl.py
@@ -88,7 +88,7 @@ async def zee5_capture(bot, update):
             return
             
     else:
-        await update.reply_text("I can download from Zee5 links only! Use @UploadHEXbot for other links 😇", quote=True)
+        await update.reply_text("I can download from Zee5 links only! Use @UploadPRObot for other links 😇", quote=True)
         return
     
     try:
